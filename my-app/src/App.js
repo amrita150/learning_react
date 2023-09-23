f2f9307293e4c377bd1d3514ed5ee86e94f5d786
amrita150 <amritakataria515@gmail.com>
2023-09-23 14:44:46 +0530

    first react program

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Card from './components/Card';
+import Heading from './components/Heading';
+
+function App() {
+  return (
+    <div>
+      <Heading></Heading>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Heading.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Heading.css';
+
+const Heading = () => {
+    return(
+        <h1 className="heading">Lorem text</h1>
+    );
+}
+
+export default Heading;
