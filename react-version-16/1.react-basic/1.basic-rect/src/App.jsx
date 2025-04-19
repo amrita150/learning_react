61b99cadea5c2dbe540fe5add916be2fda1e94fb
Amrita <amritakataria515@gmail.com>
2025-04-19 11:46:41 +0530

    react basic

---
+++ react-version-16/1.react-basic/1.basic-rect/src/App.jsx
@@ -0,0 +1,8 @@
+import Greet from './Welcome';
+
+function App(){
+  return (
+  <Greet></Greet>
+)
+}
+export default App;
---
+++ react-version-16/1.react-basic/1.basic-rect/src/Welcome.jsx
@@ -0,0 +1,12 @@
+const appName = "studySync";
+
+function Greet() {
+    return (
+        <div>
+            <h1>Welcome to {appName}</h1>
+            <p>Your verified college study space</p>
+        </div>
+    )
+}
+
+export default Greet;
